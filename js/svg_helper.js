fd939382dc8a90f32210e52706bb30213e7529ab
starry <cult.of.starry.wisdom@gmail.com>
2021-04-25 21:03:09 +0100

    Add svg_helper

---
+++ js/svg_helper.js
@@ -0,0 +1,53 @@
+// there may well be a good library for this
+// as of the time of writing this I have not written much javascript code
+// for now being able to peel away the layers and control the svg being created is valuable
+// this may well change at a later date
+// this is intended to not have any real logic within it, and be possible to switch out if I learn/am tought a better library / way
+class _svg_helper {
+	create_svg(width,height,viewport_x,viewport_y,viewport_width,viewport_height) {
+		const svgData = document.createElementNS("http://www.w3.org/2000/svg","svg");
+		svgData.setAttribute("id","svgData");
+		svgData.setAttribute("version","1.1");
+		svgData.setAttribute("width",width);
+		svgData.setAttribute("height",height);
+		svgData.setAttribute("viewBox",""+viewport_x+","+viewport_y+",+"+viewport_width+",+"+viewport_height);
+		return svgData;
+	}
+	create_circle(cx,cy,r) {
+		const circle=document.createElementNS("http://www.w3.org/2000/svg","circle");
+		circle.setAttribute("cx",cx);
+		circle.setAttribute("cy",cy);
+		circle.setAttribute("r",r);
+		return circle;
+	}
+	create_path(d) {
+		const path=document.createElementNS("http://www.w3.org/2000/svg","path");
+		path.setAttribute("d",d);
+		return path;
+	}
+	create_line(x1,y1,x2,y2) {
+		const line=document.createElementNS("http://www.w3.org/2000/svg","line");
+		line.setAttribute("x1",x1);
+		line.setAttribute("y1",y1);
+		line.setAttribute("x2",x2);
+		line.setAttribute("y2",y2);
+		return line;
+	}
+	create_g(fill,color) {
+		const g=document.createElementNS("http://www.w3.org/2000/svg","g");
+		g.setAttribute("fill",fill);
+		g.setAttribute("stroke",color);
+		return g;
+	}
+	create_png_image(x,y,width,height,data_url) {
+		const img=document.createElementNS("http://www.w3.org/2000/svg","image");
+		img.setAttribute("x",x);
+		img.setAttribute("y",y);
+		img.setAttribute("width",width);
+		img.setAttribute("height",height);
+		img.setAttribute("href",data_url);
+		return img;
+	}
+}
+
+const svg_helper = new _svg_helper()
