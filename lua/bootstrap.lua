5b24b8a24dd2f00d98dadab29e0a90564ac4efe4
starry <cult.of.starry.wisdom@gmail.com>
2021-05-26 18:23:47 +0100

    Move more of the start code over to boostrap

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -4,16 +4,21 @@ if (getScriptStorage()._cuf_gm == nil) then
 		uploads = {
 			slots = {},
 			slot_id = 0,
+			script_functions = {},
 		},
+		upload_segment = function (slot, str)
+			slots = getScriptStorage()._cuf_gm.uploads.slots
+			slots[slot] = slots[slot] .. str
+			return slots[slot];
+		end,
+		upload_end = function (slot)
+			return load(getScriptStorage()._cuf_gm.uploads.slots[slot])()
+		end,
+		upload_start = function ()
+			local slot_id = getScriptStorage()._cuf_gm.uploads.slot_id
+			getScriptStorage()._cuf_gm.uploads.slots[slot_id] = ""
+			getScriptStorage()._cuf_gm.uploads.slot_id = slot_id + 1
+			return slot_id
+		end,
 	}
-end
-
-getScriptStorage()._cuf_gm.upload_segment = function (slot, str)
-	slots = getScriptStorage()._cuf_gm.uploads.slots
-	slots[slot] = slots[slot] .. str
-	return slots[slot];
-end
-
-getScriptStorage()._cuf_gm.upload_end = function (slot)
-	return load(getScriptStorage()._cuf_gm.uploads.slots[slot])()
 end
--- lua/in-progress/upload/start.lua
+++ lua/in-progress/upload/start.lua
@@ -1,4 +1,1 @@
-local slot_id = getScriptStorage()._cuf_gm.uploads.slot_id
-getScriptStorage()._cuf_gm.uploads.slots[slot_id] = ""
-getScriptStorage()._cuf_gm.uploads.slot_id = slot_id + 1
-return slot_id
+return getScriptStorage()._cuf_gm.upload_start();
